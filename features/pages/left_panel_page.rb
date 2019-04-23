ecf4777c093a1d9b49c8e1c33b4ffbd91d46e6e7
jsliu <jsliu521@gmail.com>
2019-04-23 14:52:33 +0800

    modify executive_insights and generae_reports element locate

--- features/pages/left_panel_page.rb
+++ features/pages/left_panel_page.rb
@@ -6,11 +6,11 @@ class LeftPanelPage
   end
 
   def executive_insights
-    @driver.find_element(:css => 'body > ul > li:nth-child(10) > img')
+    @driver.find_element(:css => 'body > ul > li:nth-child(12) > img')
   end
   
   def generate_reports
-    @driver.find_element(:css => 'body > ul > li:nth-child(15)')
+    @driver.find_element(:css => 'body > ul > li:nth-child(17)')
   end
 
 end
