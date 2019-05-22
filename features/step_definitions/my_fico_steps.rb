a4a4c993c50f9aa906c549a8041e6d61dbe69ace
jsliu <jsliu521@gmail.com>
2019-05-22 13:37:17 +0800

    cancle send my fico email

--- features/step_definitions/my_fico_steps.rb
+++ features/step_definitions/my_fico_steps.rb
@@ -11,5 +11,5 @@ end
 Then /^send the fico data result$/ do
   attch_path = RESULT_REPORT_PATH + CURRENT_DAY + '/fico_loan.xls'
   @my_fico_page.run_excel_macro(attch_path)
-  send_email_with_attach('eng-thc@thc.net.cn', 'daily get myFico data', 'get myFico data', attch_path)
+  #send_email_with_attach('eng-thc@thc.net.cn', 'daily get myFico data', 'get myFico data', attch_path)
 end
